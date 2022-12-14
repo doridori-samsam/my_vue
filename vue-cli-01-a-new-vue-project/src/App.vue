de558948067fcd7dbc94a8c7f05d703d8c192df4
Sam <wtawonlife@gmail.com>
2022-12-15 00:27:49 +0900

    vue component

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "023 393 1938",
+          email: "manuel@local.com",
+        },
+        {
+          id: "Peter",
+          name: "Peter bread",
+          phone: "223 595 2267",
+          email: "peter@local.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+  computed: {},
+};
+</script>
